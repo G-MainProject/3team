20541cac31e441a5cab7f85a7326c9163855600d
전승원 <jsw02078@sau.ac.kr>
2025-09-05 17:30:13 +0900

    style: #15 로그인페이지 element 생성(09.05)

--- React/src/pages/Login/Login.jsx
+++ React/src/pages/Login/Login.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import './Login.css';
+import logoS from '../../assets/images/logoS.png';
 
 export default function Login() {
     return (
-        <div>
-            <h1>Login Page</h1>
-            <p>로그인 페이지입니다.</p>
+        <div className='login-container'>
+            <div className='login-card'>
+                <img src={logoS} alt="logoS" />
+                <p className='login-card-title'>로그인</p>
+                <input type="text" placeholder='ID' />
+                <input type="password" placeholder='Password' />
+                <button>Login</button>
+                <div className='login-card-footer'>
+                    <p>Don't have an account? <span>Sign up</span></p>
+                </div>
+            </div>
         </div>
     );
 }
